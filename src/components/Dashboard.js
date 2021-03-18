1d7f5079653198644db1977df03990ee6464164e
sreena k <sreenanair.k@gmail.com>
2021-03-18 22:17:19 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,10 @@
-import React from "react";
+import React from 'react';
 
-const Dashboard = props => {
-  return (
-    <div>
-      <div>
-        <h1>Dashboard</h1>
-        <h1>Status: {props.loggedInStatus}</h1>
-      </div>
-    </div>
+export default function Dashboard() {
+  return(
+  	<div>
+    <h2>Dashboard</h2>
+   <h1>Status: {props.loggedInStatus}</h1>
+   </div>
   );
-};
-
-export default Dashboard;
+}
